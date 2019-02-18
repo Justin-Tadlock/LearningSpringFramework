e5b193558bb014c7e643fadcac2576cacb3aa827
JustinTadlock <justin.b.tadlock@gmail.com>
2019-02-18 13:08:25 -0700

    Implemented autowiring property injection

--- Guess-The-Number-Game/core/src/main/java/learning/springframework/GameImpl.java
+++ Guess-The-Number-Game/core/src/main/java/learning/springframework/GameImpl.java
@@ -2,6 +2,7 @@ package learning.springframework;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -10,6 +11,7 @@ public class GameImpl implements Game {
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     // Fields
+    @Autowired
     private NumberGenerator numberGenerator;
 
     private int guessCount = 10;
@@ -39,10 +41,6 @@ public class GameImpl implements Game {
     }
 
     // == Public Methods ==
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }
-
     @Override
     public int getNumber() {
         return number;
