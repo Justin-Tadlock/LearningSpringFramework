15a5cfbb208ff376581631037fbe305d0f2a519f
JustinTadlock <justin.b.tadlock@gmail.com>
2019-02-28 19:59:37 -0700

    Adding dynamic initializing WebApplication configurations and servlets

---
+++ Todo-List/src/main/java/learning/springframework/config/WebConfig.java
@@ -0,0 +1,12 @@
+package learning.springframework.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "learning.springframework")
+public class WebConfig {
+
+}
