6541bbeeb97e0dc2f107d287419a86398a469660
Justin Tadlock <justin.b.tadlock@gmail.com>
2019-03-28 13:02:57 -0700

    Implementing SpringBoot WebApp

---
+++ Guess-The-Number-Game/web/src/main/java/learn/springframework/WebMain.java
@@ -0,0 +1,11 @@
+package learn.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMain {
+  public static void main(String[] args) {
+    SpringApplication.run(WebMain.class, args);
+  }
+}
